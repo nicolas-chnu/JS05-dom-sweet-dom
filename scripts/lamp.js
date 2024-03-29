5a6627723677027c21ebbd2d1326c260880693d7
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-03-29 15:56:42 +0200

    Refactor lamp.js

--- scripts/lamp.js
+++ scripts/lamp.js
@@ -1,91 +1,79 @@
 import {animatePro, bowShooting, bounce} from './animation.js'
 
 const duration = 1000;
-const animationLen = 400;
+const animationPathLen = 400;
 const initialLampTop = -100;
 
-let toggleLampTypeBtn = document.querySelector('.toggle-lamp-type-btn');
-let toggleLampPowerBtn = document.querySelector('.toggle-lamp-power-btn');
+const toggleLampTypeBtn = document.querySelector('.toggle-lamp-type-btn');
+const toggleLampPowerBtn = document.querySelector('.toggle-lamp-power-btn');
 
-let setLampBrightnessBtn = document.querySelector('.set-lamp-brightness-btn');
-let bulb = document.querySelector('.lamp__bulb');
-let lamp = document.querySelector('.lamp');
-let timeout = getSleepTimeout();
+const setLampBrightnessBtn = document.querySelector('.set-lamp-brightness-btn');
+const bulbElem = document.querySelector('.lamp__bulb');
+const lampElem = document.querySelector('.lamp');
+
+let timeout;
+let currentLampType = 'basic'
 
 function animateLampUp() {
     animatePro(
         bowShooting,
-        (progress) => lamp.style.top = initialLampTop - progress * animationLen + 'px',
+        (progress) => lampElem.style.top = initialLampTop - progress * animationPathLen + 'px',
         duration);
 }
 
 function animateLampDown() {
     animatePro(
         (timeFraction) => 1 - bounce(1 - timeFraction),     // bounce ease out
-        (progress) => lamp.style.top = initialLampTop - animationLen + progress * animationLen + 'px',
+        (progress) => lampElem.style.top = initialLampTop - animationPathLen + progress * animationPathLen + 'px',
         duration);
-}
-
-function getSleepTimeout() {
-    return setTimeout(() => bulb.classList.remove('on'), 5000);
 }
 
 function updateSleepTimeout() {
     clearTimeout(timeout)
-    timeout = getSleepTimeout();
+    timeout = setTimeout(() => bulbElem.classList.remove('is-on'), 5000);
 }
 
-function getCurrentLampType() {
-    return lamp.getAttribute('lamp-type')
+function updateSetLampBrightnessBtn() {
+    setLampBrightnessBtn.disabled = currentLampType !== 'led';
 }
 
-function updateSetBrightnessAbility(type) {
-    setLampBrightnessBtn.disabled = type !== 'led';
+function switchToNextLamp() {
+    currentLampType = getNextLampType()
+    updateSetLampBrightnessBtn();
+
+    bulbElem.nextElementSibling.style.opacity = '1';
+    lampElem.setAttribute('lamp-type', currentLampType)
 }
 
-function switchLampType() {
-    let current = getCurrentLampType();
-    let newType = getNextLampType(current);
-
-    animateLampUp();
-
-    setTimeout(() => {
-        updateLampType(current, newType);
-        animateLampDown();
-    }, duration + 500);
-}
-
-function updateLampType(before, after) {
-    updateSetBrightnessAbility(after);
-
-    bulb.nextElementSibling.style.opacity = '1';
-    lamp.setAttribute('lamp-type', after)
-}
-
-function getNextLampType(currentType) {
+function getNextLampType() {
     const lampTypes = ['basic', 'eco', 'led'];
-    return lampTypes[(lampTypes.indexOf(currentType) + 1) % (lampTypes.length)];
-}
-
-function promptLampBrightness() {
-    return Math.max(0, Math.min(+prompt('Enter brightness from 0 to 100'), 100)) / 100;
+    return lampTypes[(lampTypes.indexOf(currentLampType) + 1) % (lampTypes.length)];
 }
 
 function updateBrightness() {
-    bulb.nextElementSibling.style.opacity = promptLampBrightness().toString();
+    let brightness = +prompt('Enter brightness from 0 to 100')
+    let coercedBrightness = Math.max(0, Math.min(brightness, 100)) / 100;
+
+    bulbElem.nextElementSibling.style.opacity = coercedBrightness.toString();
 }
 
-window.onmousemove = () => updateSleepTimeout();
-window.onmousedown = () => updateSleepTimeout();
-window.onkeydown = () => updateSleepTimeout();
-window.onwheel = () => updateSleepTimeout();
+window.onmousemove = updateSleepTimeout;
+window.onmousedown = updateSleepTimeout;
+window.onkeydown = updateSleepTimeout;
+window.onwheel = updateSleepTimeout;
 
 window.onload = () => {
-    updateSetBrightnessAbility(getCurrentLampType());
+    updateSetLampBrightnessBtn();
     setTimeout(animateLampDown, 500);
 }
 
-toggleLampTypeBtn.onclick = () => switchLampType()
-toggleLampPowerBtn.onclick = () => bulb.classList.toggle('on');
+toggleLampTypeBtn.onclick = () => {
+    animateLampUp();
 
-setLampBrightnessBtn.onclick = () => updateBrightness();
+    setTimeout(() => {
+        switchToNextLamp()
+        animateLampDown();
+    }, duration + 200);
+}
+toggleLampPowerBtn.onclick = () => bulbElem.classList.toggle('is-on')
+setLampBrightnessBtn.onclick = updateBrightness;
